d1d01c00417e5bad32fcc3d760e73ccbda5b75f6
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-06 10:15:53 +0100

    initial commit

---
+++ lib/greeting_service_kata.ex
@@ -0,0 +1,18 @@
+defmodule GreetingServiceKata do
+  @moduledoc """
+  Documentation for `GreetingServiceKata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GreetingServiceKata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
