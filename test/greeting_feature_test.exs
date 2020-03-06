b184d316a76ead2fb05a66615a8eb3c2dfe6e28b
Giacomo Montibeller <giacomo.montibeller@xpeppers.com>
2020-03-06 12:07:50 +0100

    include user name in greeting message

--- test/greeting_feature_test.exs
+++ test/greeting_feature_test.exs
@@ -11,4 +11,15 @@ defmodule GreetingFeatureTest do
       assert response.body == "Hello my friend!"
     end
   end
+
+  describe "When a User with the name Pippo request a greeting message" do
+    test "Then the system will reply with 'Hello Pippo!'" do
+      GreetingServiceKata.Application.start(nil, [])
+
+      response = HTTPoison.get!("http://localhost:4000/greet?name=Pippo")
+
+      assert response.status_code == 200
+      assert response.body == "Hello Pippo!"
+    end
+  end
 end
